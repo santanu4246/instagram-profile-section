86c4277aff68cdbecf4d5a9cd47b3db42e06f363
Santanu Dutta <155149934+santanu4246@users.noreply.github.com>
2024-04-13 19:53:47 +0530

    form validation

---
+++ script.js
@@ -0,0 +1,26 @@
+const form = document.querySelector('.input');
+const input = document.querySelector('#input');
+// console.log(input);
+const password = document.querySelector('#password');
+console.log(password);
+
+input.addEventListener('input', (e) => {
+    const inputValue = e.target.value; // Trim whitespace from input value
+    
+    if (inputValue.includes(' ')) {
+        document.querySelector('.username').innerText = 'Please enter a valid username';
+    } else {
+        document.querySelector('.username').innerText = ''; // Clear error message if input is valid
+    }
+});
+password.addEventListener('input',(e)=>{
+    const inputValue = e.target.value;
+
+    if (inputValue.length <= 6) {
+        document.querySelector('.pass').innerText = 'Password must be greater than 6 characters';
+    } else if (inputValue.includes(' ')) {
+        document.querySelector('.pass').innerText = 'Password cannot contain spaces';
+    } else {
+        document.querySelector('.pass').innerText = ''; // Clear error message if input is valid
+    }
+});
