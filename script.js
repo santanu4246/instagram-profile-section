14f1cc0f32064a1984a1fd8b3cbaf646bc13aeed
Santanu Dutta <155149934+santanu4246@users.noreply.github.com>
2024-04-15 17:23:04 +0530

    reels section

--- script.js
+++ script.js
@@ -3,17 +3,19 @@ const input = document.querySelector('#input');
 // console.log(input);
 const password = document.querySelector('#password');
 console.log(password);
-
+setInterval(() => {
+    abc()
+}, 100);
 input.addEventListener('input', (e) => {
     const inputValue = e.target.value; // Trim whitespace from input value
-    
+
     if (inputValue.includes(' ')) {
         document.querySelector('.username').innerText = 'Please enter a valid username';
     } else {
         document.querySelector('.username').innerText = ''; // Clear error message if input is valid
     }
 });
-password.addEventListener('input',(e)=>{
+password.addEventListener('input', (e) => {
     const inputValue = e.target.value;
 
     if (inputValue.length <= 6) {
@@ -24,3 +26,30 @@ password.addEventListener('input',(e)=>{
         document.querySelector('.pass').innerText = ''; // Clear error message if input is valid
     }
 });
+
+
+function abc() {
+    const videos = document.querySelectorAll(".video")
+    videos.forEach((v) => {
+        if (v.getBoundingClientRect().y >= -400 && v.getBoundingClientRect().y <= 400) {
+            v.play()
+            // v.muted = false;
+        } else {
+            v.pause()
+        }
+    })
+}
+
+
+
+videos.forEach(video => {
+    video.addEventListener('click', () => {
+        console.log(video);
+        if (video.paused) {
+            video.play();
+        } else {
+            video.pause();
+        }
+    });
+});
+
